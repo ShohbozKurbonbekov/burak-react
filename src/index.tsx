388d13c25b3318d0a0135703f9d4e29e2b4c5a59
Kurbonbekov Shohboz <skurbonbekov98@gmail.com>
2025-04-16 00:48:28 +0900

    feat: Material UI integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
+import theme from "./theme";
 import reportWebVitals from "./reportWebVitals";
 import "./css/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
