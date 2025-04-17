25e494a01dea47dc5ba3162baa4421fe46b5a58b
Kurbonbekov Shohboz <skurbonbekov98@gmail.com>
2025-04-17 22:47:12 +0900

    feat: creating a routing system

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,40 @@
 import React from "react";
 import "../css/app.css";
-import { Container, Stack, Box, Typography, Button } from "@mui/material";
-import { RippleBadge } from "./MaterialTheme/styled";
+import { Switch } from "react-router-dom";
+
+import { Link, Route } from "react-router-dom";
+import { About } from "./screens/HomePage/Index";
+import { Users } from "./screens/Users";
+import { Home } from "./screens/Home";
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Stack flexDirection={"column"}>
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component={"h4"}>
-            Create React Application with Typescript and Redux
-          </Typography>
-        </Box>
-        <Box>
-          <RippleBadge badgeContent={1997}>
-            <Button variant="contained">Contained</Button>
-          </RippleBadge>
-        </Box>
-      </Stack>
-    </Container>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,16 @@ import App from "./app/App";
 import theme from "./app/MaterialTheme";
 import reportWebVitals from "./reportWebVitals";
 import "./css/index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <Router>
+          <App />
+        </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
