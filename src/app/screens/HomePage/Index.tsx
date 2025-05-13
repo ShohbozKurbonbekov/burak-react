98fa6da762609a5b8c625940d2fe454b5c66e1ac
Kurbonbekov Shohboz <skurbonbekov98@gmail.com>
2025-05-14 01:55:53 +0900

    feat: creating homePage Redu x configuration

--- src/app/screens/HomePage/Index.tsx
+++ src/app/screens/HomePage/Index.tsx
@@ -5,8 +5,16 @@ import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/homePage.css";
+import { useEffect } from "react";
 
 export default function HomePage() {
+  // Select: data => store
+
+  useEffect(() => {
+    // Backend server request => data
+    // Slice data => store
+  }, []);
+
   return (
     <div className="homePage">
       <Statistics />
