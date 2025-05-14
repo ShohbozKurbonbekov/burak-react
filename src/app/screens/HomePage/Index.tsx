a062c6b2874e91d1a2daedde5adb898e666d1a59
Kurbonbekov Shohboz <skurbonbekov98@gmail.com>
2025-05-14 14:25:08 +0900

    feat: developing slice and  selector usage

--- src/app/screens/HomePage/Index.tsx
+++ src/app/screens/HomePage/Index.tsx
@@ -6,8 +6,26 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/homePage.css";
 import { useEffect } from "react";
+import { Dispatch } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+
+// REDUX SLICE & SELECTOR
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
+
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({ popularDishes })
+);
 
 export default function HomePage() {
+  const { setPopularDishes } = actionDispatch(useDispatch());
+  const { popularDishes } = useSelector(popularDishesRetriever);
   // Select: data => store
 
   useEffect(() => {
